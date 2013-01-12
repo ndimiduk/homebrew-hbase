8d8f2f88816e3c3a5b4f1bd3d8529ce2a7e1b687
Nick Dimiduk <ndimiduk@gmail.com>
2013-01-11 19:06:59 -0800

    add HBase 0.94.x
    add recipe for 0.94.x at version 0.94.3

---
+++ hbase-0.94.rb
@@ -0,0 +1,45 @@
+require 'formula'
+
+class Hbase094 < Formula
+  url 'http://www.apache.org/dyn/closer.cgi?path=hbase/hbase-0.94.3/hbase-0.94.3.tar.gz'
+  homepage 'http://hbase.apache.org'
+  md5 '8271bd75c4a4e63c0c435b10e4861701'
+
+  depends_on 'hadoop'
+
+  def shim_script target
+    <<-EOS.undent
+      #!/bin/bash
+      exec "#{libexec}/bin/#{target}" "$@"
+    EOS
+  end
+
+  def install
+    rm_f Dir["bin/*.bat"]
+    libexec.install %w[bin conf docs lib hbase-webapps]
+    libexec.install Dir['*.jar']
+    bin.mkpath
+    Dir["#{libexec}/bin/*"].each do |b|
+      n = Pathname.new(b).basename
+      (bin+n).write shim_script(n)
+    end
+
+    inreplace "#{libexec}/conf/hbase-env.sh",
+      "# export JAVA_HOME=/usr/java/jdk1.6.0/",
+      "export JAVA_HOME=\"$(/usr/libexec/java_home)\""
+  end
+
+  def caveats; <<-EOS.undent
+    Requires Java 1.6.0 or greater.
+
+    You must also edit the configs in:
+      #{libexec}/conf
+    to reflect your environment. If you're using multiple hbase versions,
+    be sure to give each its own hbase.rootdir and hbase.tmp.dir to avoid
+    conflicts in persisted data.
+
+    For more details:
+      http://wiki.apache.org/hadoop/Hbase
+    EOS
+  end
+end
